44ccfaff3b0de52cb54ad2544823aa4b639a4802
IvanZhmurko <djvanya79@gmail.com>
2023-10-06 00:00:07 +0300

    fix

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 
+
 export class Item extends Component {
   render() {
     return (
-        <div className="item">
-          <img src={"./img/" + this.props.item.img} alt= "Фотографія гітари" onClick={()=> this.props.onShowItem(this.props.item)}/>
+       <div className='item'>
+          <img src={'./img/' + this.props.item.img}
+             alt="Фотографія гітари"
+             onClick={()=> this.props.onShowItem(this.props.item)}/>
           <h2>{this.props.item.title}</h2>
           <p>{this.props.item.desc}</p>
+          <p>foto</p>
           <b>{this.props.item.price}$</b>
-          <div className='add-to-cart'onClick={()=>this.props.onAdd(this.props.item)}>+</div>
+          <div
+             className="add-to-cart"
+             onClick={() => this.props.onAdd(this.props.item)}
+          >
+             +
+          </div>
        </div>
     );
   }
